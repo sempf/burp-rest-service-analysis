c96eacd7dd344d493bd2f2dc9324558502b52ff4
bill@pointweb.net <bill@pointweb.net@e4df1efa-36cb-02d9-512e-172457980bbf>
2014-01-09 22:53:19 +0000

    Added initial API Secret In URL tests.

--- trunk/src/burp/BurpExtender.java
+++ trunk/src/burp/BurpExtender.java
@@ -12,12 +12,15 @@ import java.util.regex.Pattern;
 /**
  * Burp Extender to analyze the output from REST services for common vulnerabilities
  * 
- * Some examples: 
- * <li>Apache Tomcat/6.0.24 - Error report
- * <li>Server: Apache/2.2.4 (Unix) mod_perl/2.0.3 Perl/v5.8.8
- * <li>X-AspNet-Version: 4.0.30319
- * 
- * @author Bill Sempf <bill@pointweb.net>
+ * Code Features: 
+ * Check to make sure the API secret isn't passed in
+ * Check for the existence of a CSRF Token
+ * Check all HTTP Verbs
+ * Check for direct object references by iterating through URL parameters
+ * Check for mass-assignment vulnerability
+ * Check for auto select parameters
+
+* @author Bill Sempf <bill@pointweb.net>
  */
 public class BurpExtender implements IBurpExtender, IScannerCheck {
 
@@ -25,8 +28,20 @@ public class BurpExtender implements IBurpExtender, IScannerCheck {
     private IExtensionHelpers helpers;
     private OutputStream output;
     
-         
-    
+    //Check for apisecret
+    //This is hard, but I can look for the common variable names
+    private static final Pattern FACEBOOK_API = Pattern.compile("&[aA][pP][pP][sS][eE][cC][rR][eE][tT]=");
+    private static final Pattern FACEBOOK_API_2 = Pattern.compile("&[aA][pP][pP]_[sS][eE][cC][rR][eE][[tT]=");
+    private static final Pattern GOOGLE_API = Pattern.compile("&[aA][pP][iI][sS][eE][cC][rR][eE][tT]=");
+    private static final Pattern GOOGLE_API_2 = Pattern.compile("&[aA][pP][iI]_[sS][eE][cC][rR][eE][tT]=");
+
+    private static final List<MatchRule> rules = new ArrayList<MatchRule>();
+    static {
+	rules.add(new MatchRule(FACEBOOK_API, 1, "Facebook API Secret"));
+	rules.add(new MatchRule(FACEBOOK_API_2, 1, "Facebook API Secret"));
+	rules.add(new MatchRule(GOOGLE_API, 1, "Google API Secret"));
+	rules.add(new MatchRule(GOOGLE_API_2, 1, "Google API Secret"));
+    }
     /**
      * implement IBurpExtender
      */
@@ -55,7 +70,72 @@ public class BurpExtender implements IBurpExtender, IScannerCheck {
     */
     @Override
     public List<IScanIssue> doPassiveScan(IHttpRequestResponse baseRequestResponse) {
-        return null;
+
+        //Secrets in the URL can be here.
+        List<ScannerMatch> matches = new ArrayList<ScannerMatch>();
+	List<IScanIssue> issues = new ArrayList<IScanIssue>();
+
+	//get the URL of the requst
+	URL url = helpers.analyzeRequest(baseRequestResponse).getUrl();
+	println("Scanning for API Secrets in the URL: " + url.toString());
+        
+	for (MatchRule rule : rules) {
+	    Matcher matcher = rule.getPattern().matcher(url.toString());
+	    while (matcher.find()) {
+		println("FOUND " + rule.getType() + "!");
+		
+		//get the actual match 
+		String group;
+		if (rule.getMatchGroup() != null) {
+		    group = matcher.group(rule.getMatchGroup());
+		} else {
+		    group = matcher.group();
+		}
+
+		println("start: " + matcher.start() + " end: " + matcher.end() + " group: " + group);
+
+		matches.add(new ScannerMatch(matcher.start(), matcher.end(), group, rule.getType()));
+	    }
+	}
+
+        	// report the issues ------------------------
+	if (!matches.isEmpty()) {
+	    Collections.sort(matches);  //matches must be in order 
+	    StringBuilder description = new StringBuilder(matches.size() * 256);
+	    description.append("Values that are labeled as API secrets are appearing in the URLs or ReST service calls.<br>");
+	    description.append("The API Secret should be kept out of direct requests to the API. Any value in a URL can be intercepted by and attacker, even under SSL. URLs with parameters are regularly cached in routers, servers, and bookmark lists.<br><br>");
+	    description.append("The following API Secrets appear to be in the URL:<br><br>");
+	    
+	    List<int[]> startStop = new ArrayList<int[]>(1);
+	    for (ScannerMatch match : matches) {
+		println("Processing match: " + match);
+		println("    start: " + match.getStart() + " end: " + match.getEnd() + " match: " + match.getMatch() + " match: " + match.getMatch());
+
+		//add a marker for code highlighting
+		startStop.add(new int[]{match.getStart(), match.getEnd()});
+
+		//add a description
+		description.append("<li>");
+
+		description.append(match.getType()).append(": ").append(match.getMatch());
+
+	    }
+
+	    println("    Description: " + description.toString());
+
+	    issues.add(new CustomScanIssue(
+			baseRequestResponse.getHttpService(),
+			helpers.analyzeRequest(baseRequestResponse).getUrl(),
+			new IHttpRequestResponse[]{callbacks.applyMarkers(baseRequestResponse, null, startStop)},
+			"ReST API Secret found in URL",
+			description.toString(),
+			"High",
+                        "Firm"));
+
+	    println("issues: " + issues.size());
+
+        }
+    	return issues;
     }
 
     @Override
